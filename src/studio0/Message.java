a4f3e9bb6c795eec879abfa300be5da623229cae
sanvikm <sanvikm@localhost>
2025-01-15 15:14:40 -0600

    i think we got it now

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println(" Hello, I want this to work ");
+		System.out.println(" I think we solved it now ");
 	}   
 }
